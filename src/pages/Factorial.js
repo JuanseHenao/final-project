2a22163dcf1812cad41d30673652285cd5e36ba1
juansehenao <juanse46101@gmail.com>
2021-11-13 23:31:31 -0500

    integration

--- src/pages/Factorial.js
+++ src/pages/Factorial.js
@@ -9,30 +9,28 @@ class Factorial extends Component {
     constructor() {
         super();
         this.state={
-            radio: 0,
-            perimetro: 0,
-            area: 0,
+            numero: 0,
+            fact: 0,
         }
         this.calculate=this.calculate.bind(this)
     }
 
     async calculate(e){
         e.preventDefault()
-        const url = "https://finalprojectbackjuansehenao.herokuapp.com/api/arpercir"
+        const url = "https://finalprojectbackjuansehenao.herokuapp.com/api/factorial"
         const config = {
             method: 'get',
             url: url,
             headers:{
-                'radio': this.state.radio,
-                'area': this.state.area,
-                'perim': this.state.perimetro
+                'numero': this.state.numero,
+                'fact': this.state.fact
             }}
 
         const response = await Axios(config)
 
         const data = response.data
         console.log(data)
-        this.setState({area:data.area,perimetro:data.perim})
+        this.setState({fact:data.fact})
     }
 
     render() {
@@ -41,11 +39,15 @@ class Factorial extends Component {
                 <div className="menuC">
                     <h1>Factorial</h1>
                     <p>Ingrese el número del que deseas conocer su factorial</p>
-                    <form>
-                        <input type="number"/>
-                        <button>Mostrar</button>
+                    <form onSubmit={this.calculate}>
+                        <input type="number" onChange={e => this.setState({numero:e.target.value})}/>
+                        <button type="submit">Mostrar</button>
                     </form>
-                    <h4>Aquí ira el resultado</h4>
+                    {this.state.fact >= 0 && (
+                        <div>
+                            <h4>Resultado: {this.state.fact}</h4>
+                        </div>
+                    )}
                     <Link to="/"><button>Volver al Menu</button></Link>
                 </div>
             </div>
